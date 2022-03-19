a8852c92078868257ceedaa71ae197a2228405c8
Kirito Huang <kirito_huang@163.com>
2022-03-19 13:03:02 +0800

    clear project files

--- Projects/PC/wysc/js/for_ie.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.category .item').hover(function () {
-        $(this).find('.cate_img').animate({"bottom": 0}, 200);
-        $(this).find('.arrow-r').animate({"right": "55px"}, 200);
-    }, function () {
-        $(this).find('.cate_img').animate({"bottom": "-10px"}, 200);
-        $(this).find('.arrow-r').animate({"right": "65px"}, 200);
-    });
-});
